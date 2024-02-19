dfd1c43d32e1d128cae83d19eba4a881fe8da120
esa-g <159166073+esa-g@users.noreply.github.com>
2024-02-19 20:40:43 +0100

    Added basic search engine (w/o API)

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function SearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", SearchSubmit);
